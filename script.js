750274013eec74ad3b68d8745ee283f526db13fe
khaly <khalyruot12345@gmail.com>
2024-10-22 11:27:06 +0700

    add class

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("a");
+
+class Node{
+
+    constructor(data,next=null){
+        this.data = data;
+        this.next = next;
+    }
+
+}
+
+
+class LinkedList{
+
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    //prepend(value) adds a new node containing value to the start of the list
+    prependFist(data){
+
+    }
+
+    //append(value) adds a new node containing value to the end of the list
+    appendEnd(data){
+        this.data = new Node(data);
+
+    }
+}
+
